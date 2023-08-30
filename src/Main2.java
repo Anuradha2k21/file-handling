9fc548cc4edeea6e9ba278c42249dd38794580d4
Anuradha2k21 <61109105+Anuradha2k21@users.noreply.github.com>
2023-08-30 14:32:51 +0530

    Update Main2.java
    poem.txt is file name

--- src/Main2.java
+++ src/Main2.java
@@ -1,3 +1,17 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main2 {
-    
+    public static void main(String[] args) {
+        try {
+            FileWriter file = new FileWriter("poem.txt");
+
+//            file.write("Hello how are you \nwtf man");    //  this overrides what's in the file
+            file.append("\ncode by anuradha");      //  this adds to the end
+            file.append("\nNigga");
+            file.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
