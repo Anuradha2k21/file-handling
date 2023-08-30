d4712d0795845e472a3d367bf990f0b8a1aec01d
Anuradha2k21 <61109105+Anuradha2k21@users.noreply.github.com>
2023-08-30 14:07:24 +0530

    Update Main.java
    get file path

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
         //  if it's "/", a single "/" would be enough, but if it's "\" it needs to be "\\"
         //  if the file is in project folder it only needs the file name
-        File file=new File("C:\\Users\\anura\\OneDrive/Desktop/message.txt");      //  create an object with a file name parameter
+        File file=new File("message.txt");      //  create an object with a file name parameter
 
         if(file.exists()){
             System.out.println("File exists");
+
+            System.out.println(file.getPath());     //  this displays whatever in the constructor
+            System.out.println(file.getAbsoluteFile());     //  displays the real path of the file in the computer
         } else {
             System.out.println("that file does not exist");
         }
