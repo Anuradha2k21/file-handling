e509b486130a8e30f9dbf8c42ed39e7bcb354ff2
Anuradha2k21 <61109105+Anuradha2k21@users.noreply.github.com>
2023-08-30 14:10:54 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,9 @@ public class Main {
 
             System.out.println(file.getPath());     //  this displays whatever in the constructor
             System.out.println(file.getAbsoluteFile());     //  displays the real path of the file in the computer
+            System.out.println(file.isFile());      //  verify a file is a file (whether it's a file or a directory)
+
+//            file.delete();  //  deletes the file
         } else {
             System.out.println("that file does not exist");
         }
